b62217a8d0747e39cfbcfcd8d66371ed11e6ffc6
anzai.k <anzai.k@opst.co.jp>
2020-07-12 19:29:38 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+exports.handler = (event, context, callback) => {
+  const request = require('request');
+
+  var payload = JSON.parse(JSON.parse(event.toString()).payload);
+
+  var URL = 'https://slack.com/api/conversations.list';
+
+  request.get({
+      uri: URL,
+      headers: {'Content-type': 'application/x-www-form-urlencoded'},
+      qs: {'token': payload.token, 'exclude_archived': 'true'},
+  }, function(err, req, data){
+
+      var channels = []
+      JSON.parse(data).channels.forEach(channel => {channels.push(channel)});
+
+      var text = '興味のあるチャンネルに参加してみよう。\n使っていないチャンネルは削除するので Slack管理者 までご連絡ください。\n\n'
+      channels.forEach(channel => {text += '<https://opst.slack.com/archives/' + channel.id + '|#' + channel.name + '>\n';});
+
+      var URL = 'https://slack.com/api/chat.postMessage';
+
+      request.post({
+        uri: URL,
+        headers: {'Content-type': 'application/json'},
+        qs: {"token": payload.token, "channel": payload.channel, "text": text, "as_user": "true", "parse": "none"},
+      }, function(err, req, data){
+        callback(null, data);
+      });
+  })
+};
